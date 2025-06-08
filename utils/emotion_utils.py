40b09e233773f6e445ffc09953c839c5d063a6d6
林湘婷 <116503010+xiangtinglin@users.noreply.github.com>
2025-06-08 22:25:26 +0800

    Update emotion_utils.py

--- utils/emotion_utils.py
+++ utils/emotion_utils.py
@@ -1,1 +1,12 @@
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+import os
+
+def analyze_sentiment(text):
+    client = TextAnalyticsClient(
+        endpoint=os.environ["TEXT_ENDPOINT"],
+        credential=AzureKeyCredential(os.environ["TEXT_KEY"])
+    )
+    result = client.analyze_sentiment(documents=[text])[0]
+    return result.sentiment
 
